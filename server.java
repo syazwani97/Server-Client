dcdceb93d4f63213ba22943137c793c84f64c584
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-19 23:21:30 +0800

    Update server.java

--- server.java
+++ server.java
@@ -1,25 +1,61 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Date;
-
-/**
- * A simple TCP server. When a client connects, it sends the client the current
- * datetime, then closes the connection. This is arguably the simplest server
- * you can write. Beware though that a client has to be completely served its
- * date before the server will be able to handle another client.
- */
-public class DateServer {
-    public static void main(String[] args) throws IOException {
-        try (var listener = new ServerSocket(59090)) {
-            System.out.println("The date server is running...");
-            while (true) {
-                try (var socket = listener.accept()) {
-                    var out = new PrintWriter(socket.getOutputStream(), true);
-                    out.println(new Date().toString());
-                }
-            }
-        }
-    }
-}
+import java.net.*; 
+import java.io.*; 
+  
+public class Server 
+{ 
+    //initialize socket and input stream 
+    private Socket          socket   = null; 
+    private ServerSocket    server   = null; 
+    private DataInputStream in       =  null; 
+  
+    // constructor with port 
+    public Server(int port) 
+    { 
+        // starts server and waits for a connection 
+        try
+        { 
+            server = new ServerSocket(port); 
+            System.out.println("Server started"); 
+  
+            System.out.println("Waiting for a client ..."); 
+  
+            socket = server.accept(); 
+            System.out.println("Client accepted"); 
+  
+            // takes input from the client socket 
+            in = new DataInputStream( 
+                new BufferedInputStream(socket.getInputStream())); 
+  
+            String line = ""; 
+  
+            // reads message from client until "Over" is sent 
+            while (!line.equals("Over")) 
+            { 
+                try
+                { 
+                    line = in.readUTF(); 
+                    System.out.println(line); 
+  
+                } 
+                catch(IOException i) 
+                { 
+                    System.out.println(i); 
+                } 
+            } 
+            System.out.println("Closing connection"); 
+  
+            // close connection 
+            socket.close(); 
+            in.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Server server = new Server(5000); 
+    } 
+} 
