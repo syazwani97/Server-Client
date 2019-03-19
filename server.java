a0fc449a20c9415abd6828de474e213a5cc66ea7
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-19 21:13:21 +0800

    Update server.java

--- server.java
+++ server.java
@@ -1,37 +1,59 @@
-public class GreetServer 
+import java.net.*;
+import java.io.*;
+
+public class SimpleServer extends Thread
 {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
- 
-    public void start(int port) 
+   private ServerSocket serverSocket;
+   String clientmsg = "";
+
+   public SimpleServer(int port) throws IOException
+   {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setSoTimeout(10000);
+   }
+
+    public void run()
     {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
-            if ("hello server".equals(greeting)) 
+        while(true)
+        {
+            try
             {
-                out.println("hello client");
+                System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+                System.out.println("Just connected to " + server.getRemoteSocketAddress());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                out.writeUTF("Thank you for connecting to "
+                + server.getLocalSocketAddress() + "\nGoodbye!");
+                server.close();
             }
-            else 
+
+            catch(SocketTimeoutException s)
             {
-                out.println("unrecognised greeting");
+                System.out.println("Socket timed out!");
+                break;
             }
+
+            catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+        }
     }
- 
-    public void stop() 
+    public static void main(String [] args)
     {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-    }
-    public static void main(String[] args)
-    {
-        GreetServer server=new GreetServer();
-        server.start(6666);
+        int port = Integer.parseInt(args[0]);
+        try
+        {
+            Thread t = new SimpleServer(port);
+            t.start();
+        }
+
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
