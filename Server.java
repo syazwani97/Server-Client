e8f83d609ec6bed74e95daf0e47792a821a5a0a6
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 15:45:06 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -14,7 +14,7 @@ public class Server
         // starts server and waits for a connection 
         try
         { 
-            server = new ServerSocket(port); 
+            server = new ServerSocket(5000); 
             System.out.println("Server started"); 
   
             System.out.println("Waiting for a client ..."); 
