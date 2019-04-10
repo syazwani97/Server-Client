4fffa9046327e8ea53719a8bc8eab4fb73e7e705
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-04-11 04:31:38 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,78 +1,24 @@
 import java.io.*;
 import java.net.*;
-import java.lang.*;
-public class Server {
-	public static void main(String[] args) throws IOException {
-		final int port = 4444;
-		System.out.println("Server waiting for connection on port "+port);
-		ServerSocket ss = new ServerSocket(port);
-		Socket clientSocket = ss.accept();
-		System.out.println("Recieved connection from "+clientSocket.getInetAddress()+" on port "+clientSocket.getPort());
-		//create two threads to send and recieve from client
-		RecieveFromClientThread recieve = new RecieveFromClientThread(clientSocket);
-		Thread thread = new Thread(recieve);
-		thread.start();
-		SendToClientThread send = new SendToClientThread(clientSocket);
-		Thread thread2 = new Thread(send);
-		thread2.start();
-	}}
-class RecieveFromClientThread implements Runnable
+public class myServer
+{ 
+public static void main(String[] args) throws Exception
+{ 
+ServerSocket sersock = new ServerSocket(3000);    //instruction7 System.out.println("Server ready for chatting");       //instruction 8
+Socket sock = sersock.accept( );                                //instruction 9
+ BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+OutputStream ostream = sock.getOutputStream();
+ PrintWriter pwrite = new PrintWriter(ostream, true);
+ InputStream istream = sock.getInputStream();
+ BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ String receiveMessage, sendMessage;
+  while(true)
 {
-	Socket clientSocket=null;
-	BufferedReader brBufferedReader = null;
-	
-	public RecieveFromClientThread(Socket clientSocket)
-	{
-		this.clientSocket = clientSocket;
-	}//end constructor
-	public void run() {
-		try{
-		brBufferedReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));		
-		
-		String messageString;
-		while(true){
-		while((messageString = brBufferedReader.readLine())!= null){//assign message from client to messageString
-			if(messageString.equals("EXIT"))
-			{
-				break;//break to close socket if EXIT
-			}
-			System.out.println("From Client: " + messageString);//print the message from client
-			System.out.println("Please enter something to send back to client..");
-		}
-		this.clientSocket.close();
-		System.exit(0);
-	}
-		
-	}
-	catch(Exception ex){System.out.println(ex.getMessage());}
-	}
-}//end class RecieveFromClientThread
-class SendToClientThread implements Runnable
+ if((receiveMessage = receiveRead.readLine()) != null)
 {
-	PrintWriter pwPrintWriter;
-	Socket clientSock = null;
-	
-	public SendToClientThread(Socket clientSock)
-	{
-		this.clientSock = clientSock;
-	}
-	public void run() {
-		try{
-		pwPrintWriter =new PrintWriter(new OutputStreamWriter(this.clientSock.getOutputStream()));//get outputstream
-		
-		while(true)
-		{
-			String msgToClientString = null;
-			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));//get userinput
-			
-			msgToClientString = input.readLine();//get message to send to client
-			
-			pwPrintWriter.println(msgToClientString);//send message to client with PrintWriter
-			pwPrintWriter.flush();//flush the PrintWriter
-			System.out.println("Please enter something to send back to client..");
-		}//end while
-		}
-		catch(Exception ex){System.out.println(ex.getMessage());}	
-	}//end run
+System.out.println(receiveMessage);
+sendMessage = keyRead.readLine();
+pwrite.println(sendMessage); pwrite.flush(); 
 }
-
+}
+} 
