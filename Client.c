a0944f160977e1cd0afb487d2870e309679c669e
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-04-11 04:42:06 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,51 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+
+#include <sys/types.h>
+
 #include <netinet/in.h>
-#include <arpa/inet.h>
+
+#include <netdb.h>
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
 #include <unistd.h>
-#include<string.h>
+
+#include <errno.h>
+
+ 
+
 int main()
+
 {
-     printf("this is a client program\n");
-     struct sockaddr_in myaddr,clientaddr;
-     int newsockid;
-     int len;
-     int sockid,client_add;
-     sockid=socket(AF_INET,SOCK_STREAM,0);
-     if(sockid==-1)
-     perror("socket");
-     memset(&myaddr,0,sizeof myaddr);
-     myaddr.sin_port=htons(4444);
-     myaddr.sin_family=AF_INET;
-     myaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-     len=sizeof myaddr;
-      if((bind(sockid,(struct sockaddr*)&myaddr,sizeof myaddr))==-1)
-    perror("bind");
-     int p=connect(sockid,(struct sockaddr*)&myaddr,len);
-     if(p==-1)
-     perror("connect");
-     char msg[200];
-     int i=0;
-     char c;int l=0; char *buffer=(char *)(malloc(sizeof(char)*200)); int buffsize;
-     while(i<10)
-     {l=0;
-                printf("Client: ");
-                while((c=getchar())!='\n')
-                {
-            msg[l++]=c;
-                }
-                msg[l]='\0'; l++;
-                send(sockid,msg,l,0);
-                fflush(stdin);
-                newsockid=accept(sockid,(struct sockaddr*)&clientaddr,&client_add);
-                recv(newsockid,buffer,buffsize,0);
-                   l=0;
-                 fprintf(stdout, "server: %s", buffer);
-                  printf("\n");
-                i++;
-     }
-     close(sockid);
-     return 0;
+
+        char sendMessage[512],receiveMessage[512];
+
+        int sock, result; 
+
+        struct hostent *host;
+
+        struct sockaddr_in serverAdd; 
+
+        host = gethostbyname("xxx.xx.xx.xx");     
+
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+
+            {
+
+            perror("Socket creation failed");
+
+exit(1);
+
+        }
+
+        serverAdd.sin_family = AF_INET;    
+
+        serverAdd.sin_port = htons(5000);  
+
+        serverAdd.sin_addr = *((struct in_addr *)host->h_addr);
+
+        bzero(&(serverAdd.sin_zero),8);
+
+        if (connect(sock, (struct sockaddr *)&serverAdd, sizeof(struct sockaddr)) == -1)
+
+        {
+
+            perror("Connection failed");
+
+            exit(1);
+
+        }
+
+        while(1)
+
+        {
+
+                        result = recv(sock,receiveMessage,1024,0);
+
+                        receiveMessage[result] = '\0';
+
+                        printf("\nRecieved Message: %s " , receiveMessage);
+
+                        printf("\nSEND The message: ");
+
+                        fgets(sendMessage,512,stdin);
+
+                        send(sock,sendMessage,strlen(sendMessage), 0);
+
+        }  
+
+        return 0;
+
 }
