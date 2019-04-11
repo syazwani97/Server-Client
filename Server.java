3cc2bd43efe44c325eba3db772349093a3a58a29
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-04-11 09:11:06 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -10,7 +10,7 @@ public class Server {
     //static ServerSocket variable
     private static ServerSocket server;
     //socket server port on which it will listen
-    private static int port = 9876;
+    private static int port = 4444;
     
     public static void main(String args[]) throws IOException, ClassNotFoundException{
         //create the socket server object
