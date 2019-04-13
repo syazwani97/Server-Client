1b53dd8f7f6bcbee5547322279f601cb03f05758
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-04-13 16:05:14 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.net.*;
+class Server
+{
+  public static void main(String[] args) throws Exception
+  {
+      ServerSocket sersock = new ServerSocket(5678);
+      System.out.println("Welcome to AINA'S profile! ");
+      Socket sock = sersock.accept( );                          
+                             
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwriter = new PrintWriter(ostream, true);
 
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+
+      String receiveMessage, sendMessage;               
+      while(true)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
+        {
+           System.out.println(receiveMessage);         
+        }         
+        sendMessage = keyRead.readLine(); 
+        pwriter.println(sendMessage);             
+        pwriter.flush();
+      }               
+    }                    
+}                        
