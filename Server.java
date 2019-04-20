763f67177384e72bb78005151f3534098f246b74
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-04-20 23:56:27 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,66 +1,32 @@
- import java.net.*;
  import java.io.*;
- import java.util.*;
+import java.net.*;
+public class Server
+{
+  public static void main(String[] args) throws Exception
+  {
+      ServerSocket sersock = new ServerSocket(3000);
+      System.out.println("Type your message");
+      Socket sock = sersock.accept( );                          
+                              // reading from keyboard (keyRead object)
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      // sending to client (pwrite object)
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwrite = new PrintWriter(ostream, true);
  
-   public class Server {
+                              // receiving from server ( receiveRead  object)
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
  
-      public static void main( String[] args ) {
- 
-        int port = 3000;
-      String line;
-      ServerSocket servsock;
-       Socket clisock;
-       PrintWriter output;
-
-       try {
-
-         // Create a new Server socket
-         servsock = new ServerSocket( port );
-
-         while ( true ) {
-
-           // Await a client connection
-           clisock = servsock.accept();
-
-           try {
-
-             // Create a new PrintWriter for output
-             output = new 
-                PrintWriter( clisock.getOutputStream(),
-                             true );
-
-             // Generate a time string
-             Date now = new Date();
-             String curtime = now.toString();
-
-             // Write the time out to the socket
-             output.println( curtime );
-
-             // Try to close the socket
-             try {
-               clisock.close();
-             }
-             catch( IOException e ) {
-               System.out.println( e );
-             }
-
-           }
-           catch( IOException e ) {
-             System.out.println( e );
-           }
-
-         }
-
-       }
-       catch ( UnknownHostException e ) {
-         System.out.println( e );
-         System.exit( -1 );
-       }
-       catch ( IOException e ) {
-         System.out.println( e );
-         System.exit( -1 );
-       }
-
-     }
-
-   }
+      String receiveMessage, sendMessage;               
+      while(true)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
+        {
+           System.out.println(receiveMessage);         
+        }         
+        sendMessage = keyRead.readLine(); 
+        pwrite.println(sendMessage);             
+        pwrite.flush();
+      }               
+    }                    
+}                        
